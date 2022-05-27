1da645cdd3fa15dd0985883874006b764fbbd01b
hkaya15 <kayahuseyin15@gmail.com>
2022-05-27 21:01:14 +0300

    Add 2.sql

--- Ödevler/1.sql
+++ Ödevler/1.sql
@@ -21,4 +21,4 @@ WHERE NOT length>50 AND NOT (rental_rate=2.99 OR rental_rate=4.99)
 /* OR */
 
 SELECT * FROM film
-WHERE NOT length>50 AND NOT rental_rate IN (2.99,4.99)
+WHERE NOT length>50 AND NOT rental_rate IN (2.99,4.99) *
